0a7d3111054006aeb62e969a5f0682bacf381da5
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-04-05 20:28:56 +0600

    surper Deal

---
+++ src/Component/Announcement/Announcement.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Container=styled.div`
+height: 30px;
+background-color: teal;
+color: #fff;
+display: flex;
+justify-content: center;
+font-size: 14px;
+font-weight: 500;
+align-items: center;
+
+`
+
+const Announcement = () => {
+    return (
+       <Container>Super Deal! Free Shipping on Orders Over $50</Container>
+    );
+};
+
+export default Announcement;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Announcement from '../Component/Announcement/Announcement';
 import Navbar from '../Component/Navbar/Navbar';
 
 
 const Home = () => {
     return (
         <div>
-            
+            <Announcement></Announcement>
             <Navbar/>
         </div>
     );
